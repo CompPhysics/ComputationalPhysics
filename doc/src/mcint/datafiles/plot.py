55e49f53c176851dcb665b9c0d1efeeabafd349a
mhjensen <morten.hjorth-jensen@fys.uio.no>
2016-11-10 08:04:22 +0100

    Change of website plus some test functions

--- doc/src/mcint/datafiles/plot.py
+++ doc/src/mcint/datafiles/plot.py
@@ -2,10 +2,12 @@ import numpy as np
 from  matplotlib import pyplot as plt
 # Load in data file
 data = np.loadtxt("autocor.dat")
+data1 = np.loadtxt("automersenne.dat")
 # Make arrays containing x-axis and binding energies as function of A
 x = data[:,0]
 corr = data[:,1]
-plt.plot(x, corr ,'ro')
+corr2 = data1[:,1]
+plt.plot(x, corr ,'ro', x, corr2, 'b')
 plt.axis([0,1000,-0.2, 1.1])
 plt.xlabel(r'$d$')
 plt.ylabel(r'$C_d$')
