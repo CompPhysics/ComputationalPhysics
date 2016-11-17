4ddd27218cd352bc274f3e88472939690ef25e8f
mhjensen <morten.hjorth-jensen@fys.uio.no>
2016-11-17 06:10:04 +0100

    PDE project

--- doc/src/Projects/2016/Project5/PartialDiffEquations/make.sh
+++ doc/src/Projects/2016/Project5/PartialDiffEquations/make.sh
@@ -50,7 +50,7 @@ mv -f $name.pdf ${name}.pdf
 cp $name.tex ${name}.tex
 
 # Publish
-dest=../../../../Projects/2016
+dest=../../../../../Projects/2016/Project5/
 if [ ! -d $dest/$name ]; then
 mkdir $dest/$name
 mkdir $dest/$name/pdf
