741d3d5dc80a199ea55d022f3e0280107d251eac
mhjensen <morten.hjorth-jensen@fys.uio.no>
2016-11-25 05:46:55 +0100

    Added analytical expression for local energy to MC project

--- doc/Programs/LecturePrograms/programs/PDE/cpp/MPIJacobi.cpp
+++ doc/Programs/LecturePrograms/programs/PDE/cpp/MPIJacobi.cpp
@@ -24,7 +24,13 @@ int Jacobi_P(int, int, int, mat &, vec &, vec &, double);
 int main(int argc, char *argv[]){
   int exponent; 
     string filename;
+     int totalnodes,mynode;
+     MPI_Init(&argc,&argv);
+     MPI_Comm_size(MPI_COMM_WORLD, &totalnodes);
+     MPI_Comm_rank(MPI_COMM_WORLD, &mynode);
+
     // We read also the basic name for the output file and the highest power of 10^n we want
+     if ( mynode == 0) {
     if( argc <= 1 ){
           cout << "Bad Usage: " << argv[0] <<
               " read also file name on same line and max power 10^n" << endl;
@@ -34,6 +40,8 @@ int main(int argc, char *argv[]){
         filename = argv[1]; // first command line argument after name of program
         exponent = atoi(argv[2]);
     }
+     }
+
     // Loop over powers of 10
     for (int i = 1; i <= exponent; i++){
       int  n = (int) pow(10.0,i);
@@ -48,15 +56,14 @@ int main(int argc, char *argv[]){
       n = n-1;  //  shift so that only points between endpoints are studied
 
       // start parallel region
-     int totalnodes,mynode;
-     MPI_Init(&argc,&argv);
-     MPI_Comm_size(MPI_COMM_WORLD, &totalnodes);
-     MPI_Comm_rank(MPI_COMM_WORLD, &mynode);
-    if ( mynode == 0){
-
       mat A = zeros<mat>(n,n);
       // Set up arrays for the simple case
       vec b(n);  vec x(n);
+      vec SolutionOld  = randu<vec>(n);
+      vec SolutionNew  = zeros<vec>(n);
+    if ( mynode == 0){
+
+
       A(0,1) = -1;  x(0) = h;  b(0) =  hh*f(x(0)); 
       x(n-1) = x(0)+(n-1)*h; b(n-1) = hh*f(x(n-1)); 
       for (int i = 1; i < n-1; i++){ 
@@ -66,8 +73,6 @@ int main(int argc, char *argv[]){
         A(i,i+1)  = -1.0;
       }
       A(n-2,n-1) = -1.0; A(n-1,n-2) = -1.0;
-      vec SolutionOld  = randu<vec>(n);
-      vec SolutionNew  = zeros<vec>(n);
     }
   // solve Ax = b by iteration with a random starting vector
      int maxiter = 100; double diff = 1.0; 
