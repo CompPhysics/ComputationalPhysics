a5c4ae494a49989edcd7e0c9fe6b7aa82e4c78d8
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2017-09-06 10:18:26 +0200

    added teaching yourself c+

--- doc/src/learningcpp/make.sh
+++ doc/src/learningcpp/make.sh
@@ -46,73 +46,12 @@ html=${name}-bs
 system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
 system doconce split_html $html.html --method=split --pagination --nav_button=bottom
 
-# IPython notebook
-#system doconce format ipynb $name $opt
-
-# LaTeX Beamer slides
-beamertheme=red_plain
-system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
-system pdflatex -shell-escape ${name}
-system pdflatex -shell-escape ${name}
-cp $name.pdf ${name}-beamer.pdf
-cp $name.tex ${name}-beamer.tex
-
-# Handouts
-system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-system doconce slides_beamer $name --beamer_slide_theme=red_shadow --handout
-system pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-pdfnup --nup 2x3 --frame true --delta "1cm 1cm" --scale 0.9 --outfile ${name}-beamer-handouts2x3.pdf ${name}.pdf
-rm -f ${name}.pdf
-
-# Ordinary plain LaTeX document
-rm -f *.aux  # important after beamer
-system doconce format pdflatex $name --minted_latex_style=trac --latex_admon=paragraph $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-doconce replace 'section{' 'section*{' $name.tex
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-mv -f $name.pdf ${name}-minted.pdf
-cp $name.tex ${name}-plain-minted.tex
-
-
 
 # Publish
 dest=../../pub
 if [ ! -d $dest/$name ]; then
 mkdir $dest/$name
-mkdir $dest/$name/pdf
 mkdir $dest/$name/html
-mkdir $dest/$name/ipynb
 fi
-cp ${name}*.pdf $dest/$name/pdf
 cp -r ${name}*.html ._${name}*.html reveal.js $dest/$name/html
 
-# Figures: cannot just copy link, need to physically copy the files
-if [ -d fig-${name} ]; then
-if [ ! -d $dest/$name/html/fig-$name ]; then
-mkdir $dest/$name/html/fig-$name
-fi
-cp -r fig-${name}/* $dest/$name/html/fig-$name
-fi
-
-cp ${name}.ipynb $dest/$name/ipynb
-ipynb_tarfile=ipynb-${name}-src.tar.gz
-if [ ! -f ${ipynb_tarfile} ]; then
-cat > README.txt <<EOF
-This IPython notebook ${name}.ipynb does not require any additional
-programs.
-EOF
-tar czf ${ipynb_tarfile} README.txt
-fi
-cp ${ipynb_tarfile} $dest/$name/ipynb
