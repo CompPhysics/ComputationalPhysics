9f26734df246db3eb4816da08e279ef3e7dd9c6a
mhjensen <morten.hjorth-jensen@fys.uio.no>
2019-06-17 09:17:01 +0200

    test

--- doc/Programs/LecturePrograms/programs/Classes/cpp/program11.cpp
+++ doc/Programs/LecturePrograms/programs/Classes/cpp/program11.cpp
@@ -22,13 +22,21 @@ int main (int argc, char* argv[])
   vector <vector<double> > A(n, vector<double>(n));
   vector <vector<double> > B(n, vector<double>(n));
   vector <vector<double> > C(n, vector<double>(n));
-  // Set all elements to zero  
-  for(auto& row : A) fill(row.begin(),row.end(),0.0);
+  // Set all elements to zero, this is one possible alternative
+  //  for(auto& row : A) fill(row.begin(),row.end(),0.0);
   for(auto& row : B) fill(row.begin(),row.end(),0.0);
   for(auto& row : C) fill(row.begin(),row.end(),0.0);
+  // Alternatively, we can also set the matrix elements to zero as follow
+  for(auto vec : A){
+    for(auto x : vec){  
+       x = 0.0;
+       cout << x;
+    }
+    cout << endl;
+  }
   // Set up values for matrix A and B and zero matrix C
-  for (int i = 0; i < n; i++){
-    for (int j = 0; j < n; j++) {
+  for (auto i = 0; i < n; i++){
+    for (auto j = 0; j < n; j++) {
       double angle = 2.0*M_PI*i*j/ (( double ) n);
       A[i][j] = s * ( sin ( angle ) + cos ( angle ) );
       B[j][i] =  A[i][j];
@@ -59,23 +67,6 @@ int main (int argc, char* argv[])
   cout << "  Frobenius norm  = " << Fsum << endl;
   return 0;
 }
-
-
-//  Allocating space for a double type matrix
-double ** CreateMatrix(int m, int n){
-  double ** mat;
-  mat = new double*[m];
-  for(int i=0;i<m;i++){
-    mat[i] = new double[n];
-    for(int j=0;j<m;j++)
-      mat[i][j] = 0.0;
-  }
-  return mat;
-}
-
-
- 
-
 
 
 
